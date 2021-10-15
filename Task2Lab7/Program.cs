bdba688adcbabdf0829cf8dc8cbc3c2294b3b97d
Anton Gusev <FrostTG@gmail.com>
2021-10-15 15:12:48 +0300

    Добавьте файлы проекта.

---
+++ Task2Lab7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба х=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double Sqr, Vol;
+            CulcVol(x, out Sqr, out Vol);
+            Console.WriteLine("Площадь куба равна {0:f2}\nОбъем куба равен {1:f2}", Sqr, Vol);            
+            Console.ReadKey();
+        }
+        static void CulcVol(double x, out double Sqr, out double Vol)
+        {
+            Sqr = 6 * Math.Pow(x, 2);
+            Vol = Math.Pow(x, 3);
+        }
+    }
+}
